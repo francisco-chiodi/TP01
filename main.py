fcfa8f87bbaa583460315ce212843f29d7b9b32f
lain <lain@debian.lain>
2026-04-13 18:05:00 -0400

    test

---
+++ main.py
@@ -0,0 +1,25 @@
+#TPO1
+name_lastname = input("input name and last name: ")
+ICD10_code = input("input ICD10 code: ") #H70.1
+acces_chapter_block = ICD10_code[0]
+#original_mount = int(input("input ammount"))
+chapters = "capitulo I", "capitulo II"
+
+if ICD10_code[0] ==
+    print(chapters[0])
+else:
+    print(chapters[1])
+
+"""
+CAPITULO I = Ciertas enfermedades infecciosas y parasitarias (A00–B99)
+CAPITULO II = Tumores [neoplasias] (C00–D48)
+CAPITULO III = Enfermedades de la sangre y de los órganos hematopoyéticos, 
+y ciertos trastornos que afectan el mecanismo de la inmunidad
+CAPITULO IV = Enfermedades endocrinas, nutricionales y metabólicas
+(E00–E90)
+CAPITULO V = Trastornos mentales y del comportamiento
+(F00–F99)
+"""
+
+
+
