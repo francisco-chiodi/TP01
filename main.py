367e9bd2d45b72704904723b6039d86af5b09dd4
lain <lain@debian.lain>
2026-04-13 18:57:42 -0400

    test

--- main.py
+++ main.py
@@ -13,13 +13,77 @@ else:
 
 """
 CAPITULO I = Ciertas enfermedades infecciosas y parasitarias (A00–B99)
+
 CAPITULO II = Tumores [neoplasias] (C00–D48)
-CAPITULO III = Enfermedades de la sangre y de los órganos hematopoyéticos, 
-y ciertos trastornos que afectan el mecanismo de la inmunidad
-CAPITULO IV = Enfermedades endocrinas, nutricionales y metabólicas
+
+CAPÍTULO III = Enfermedades de la sangre y de los órganos hematopoyéticos,
+ y ciertos trastornos que afectan el mecanismo de la inmunidad (D50–D89)
+ 
+CAPITULO IV  Enfermedades endocrinas, nutricionales y metabólicas
 (E00–E90)
-CAPITULO V = Trastornos mentales y del comportamiento
+
+CAPITULO V  Trastornos mentales y del comportamiento
 (F00–F99)
+Capítulo VI  Enfermedades del sistema nervioso
+(G00–G99)
+Capítulo VII Enfermedades del ojo y sus anexos
+(H00–H59)
+
+CAPÍTULO VIII Enfermedades del oído y de la apófisis mastoides
+(H60–H95)
+
+CAPÍTULO IX
+Enfermedades del sistema circulatorio (I00–I99)
+
+CAPITULO X Enfermedades del sistema respiratorio
+(J00–J99)
+
+CAPÍTULO XI Enfermedades del sistema digestivo
+(K00–K93)
+
+CAPÍTULO XII Enfermedades de la piel y del tejido subcutáneo
+(L00-L99)
+
+CAPÍTULO XIII Enfermedades del sistema osteomuscular y del tejido conjuntivo
+(M00-M99)
+
+CAPÍTULO XIV Enfermedades del sistema genitourinario
+(N00-N99)
+
+CAPÍTULO XV Embarazo, parto y puerperio
+(O00–O99)
+
+CAPÍTULO XVI Ciertas afecciones originadas en el período perinatal
+(P00–P96)
+
+CAPÍTULO XVII Malformaciones congénitas, deformidades y anomalías cromosómicas
+(Q00–Q99)
+
+CAPÍTULO XVIII
+
+Síntomas, signos y hallazgos anormales clínicos y de laboratorio, no clasificados en otra parte
+(R00–R99)
+
+ 
+CAPÍTULO XIX Traumatismos, envenenamientos y algunas otras consecuencias de causas externas
+(S00–T98)
+
+CAPÍTULO XX Causas externas de morbilidad y de mortalidad 
+(V01–Y98)
+
+CAPÍTULO XXI Factores que influyen en el estado de salud y contacto con los servicios de salud
+(Z00–Z99)
+
+Capítulo XXII Códigos para propósitos especiales
+(U00–U99)
+
+
+
+
+
+
+
+
 """
 
 
