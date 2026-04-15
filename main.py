87c70ea737b6141b79c2792a637fdad3792e99e7
francisco-chiodi <franchiodi98@gmail.com>
2026-04-15 11:38:58 -0300

    structure

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 #TPO1
 name_lastname = input("input name and last name: ")
 
-A1 = "Ciertas enfermedades infecciosas y parasitarias"
-B1 = "Ciertas enfermedades infecciosas y parasitarias"
-C1 = "Tumores [neoplasias]"
-D1 = "Tumores [neoplasias]" , "Enfermedades de la sangre y de los órganos hematopoyéticos y/n ciertos transtornos que afectan el mecanismo de la inmunidad "
-E1 = "Enfermedades endocrinas, nutricionales y metabólicas"
-F1 = "Trastornos mentales y del comportamiento"
-G1 = "Enfermedades del sistema nervioso"
-H1 = "Enfermedades del ojo y sus anexos" , "Enfermedades del oído y de la apófisis mastoides"
-I1 = "Enfermedades del sistema circulatorio"
+A1 = "CAPITULO I Ciertas enfermedades infecciosas y parasitarias"
+B1 = "CAPITULO I Ciertas enfermedades infecciosas y parasitarias"
+E1 = "CAPITULO IV Enfermedades endocrinas, nutricionales y metabólicas"
+F1 = "CAPITULO V Trastornos mentales y del comportamiento"
+G1 = "Capítulo VI Enfermedades del sistema nervioso"
+H1 = "Capítulo VII Enfermedades del ojo y sus anexos" , "Enfermedades del oído y de la apófisis mastoides"
+I1 = "CAPÍTULO VIII Enfermedades del sistema circulatorio"
+J1 = ""
 
 
 ICD10_code = input("input ICD10 code: ") #H70.1
@@ -18,8 +17,6 @@ if ICD10_code[0] == "A":
     print(A1)
 elif ICD10_code[0] == "B":
     print(B1)
-elif ICD10_code[0] == "C":
-    print(C1)
 elif ICD10_code[0] == "E":
     print(E1)
 elif ICD10_code[0] == "F":
@@ -34,63 +31,50 @@ elif ICD10_code[0] == "J":
     print(J1)
 elif ICD10_code[0] == "K":
     print(K1)
-
-
+elif ICD10_code[0] == "M":
+    print(M1)
+elif ICD10_code[0] == "L":
+    print(L1)
+elif ICD10_code[0] == "N":
+    print(N1)
+elif ICD10_code[0] == "O":
+    print(O1)
+elif ICD10_code[0] == "P":
+    print(P1)
+elif ICD10_code[0] == "Q":
+    print(Q1)
+elif ICD10_code[0] == "R":
+    print(R1)
+elif ICD10_code[0] == "S":
+    print(S1)
+elif ICD10_code[0] == "N":
+    print(N1)
+elif ICD10_code[0] == "V":
+    print(V1)
+elif ICD10_code[0] == "Z":
+    print(Z1)
+elif ICD10_code[0] == "U":
+    print(U1)
 
 if ICD10_code[0] == "D" and ICD10_code[1] == "5":
-    print("Enfermedades de la sangre y de los órganos hematopoyéticos y/n ciertos transtornos que afectan el mecanismo de la inmunidad ")
+    print("CAPÍTULO III: Enfermedades de la sangre y de los órganos hematopoyéticos y/n ciertos transtornos que afectan el mecanismo de la inmunidad ")
 else:
-    print("Tumores [neoplasias]")
-
-
-
-
-
-
-#if D[0] != 4:
-   # print(D[1])
-
-
-
-
-
-#if acces_chapter_block = a
-   # print("acces")
-#else if: acce
-
-
-
-#original_mount = int(input("input ammount"))
-#chapters = "capitulo I", "capitulo II"
+    print("CAPITULO II: Tumores [neoplasias]")
 
 
 """
-CAPITULO I = Ciertas enfermedades infecciosas y parasitarias (A00–B99) 
-
-CAPITULO II = Tumores [neoplasias] (C00–D48) 
-
-CAPÍTULO III = Enfermedades de la sangre y de los órganos hematopoyéticos,
+CAPITULO I Ciertas enfermedades infecciosas y parasitarias (A00–B99) 
+CAPITULO II Tumores [neoplasias] (C00–D48) 
+CAPÍTULO III Enfermedades de la sangre y de los órganos hematopoyéticos,
  y ciertos trastornos que afectan el mecanismo de la inmunidad (D50–D89) 
- 
-CAPITULO IV  Enfermedades endocrinas, nutricionales y metabólicas
-(E00–E90)
-
-CAPITULO V  Trastornos mentales y del comportamiento
-(F00–F99)
-Capítulo VI  Enfermedades del sistema nervioso
-(G00–G99)
-Capítulo VII Enfermedades del ojo y sus anexos
-(H00–H59)
-
-CAPÍTULO VIII Enfermedades del oído y de la apófisis mastoides
-(H60–H95)
-
-CAPÍTULO IX
-Enfermedades del sistema circulatorio (I00–I99)
-
+CAPITULO IV  Enfermedades endocrinas, nutricionales y metabólicas (E00–E90)
+CAPITULO V  Trastornos mentales y del comportamiento (F00–F99)
+Capítulo VI  Enfermedades del sistema nervioso (G00–G99)
+Capítulo VII Enfermedades del ojo y sus anexos (H00–H59)
+CAPÍTULO VIII Enfermedades del oído y de la apófisis mastoides (H60–H95)
+CAPÍTULO IX Enfermedades del sistema circulatorio (I00–I99)
 CAPITULO X Enfermedades del sistema respiratorio
 (J00–J99)
-
 CAPÍTULO XI Enfermedades del sistema digestivo
 (K00–K93)
 
