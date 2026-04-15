408a8a5d143c4808b7548ab378c06176e4759f8b
Selee-Posadas <215926670+Selee-Posadas@users.noreply.github.com>
2026-04-15 16:30:44 +0000

    Agrego print de salida

--- sele.py
+++ sele.py
@@ -1,4 +1,4 @@
-paciente = input('')
+beneficiario = input('')
 codigo_icd10 = input('')
 monto_base = int(input(''))
 
@@ -6,54 +6,57 @@ monto_base = int(input(''))
 letra = codigo_icd10[0]
 bloque = int(codigo_icd10[1:3])
 
-if letra == 'A' or letra == 'B':
+if letra == "A" or letra == "B":
     capitulo = "Capítulo I: Ciertas enfermedades infecciosas y parasitarias"
-elif letra == 'C':
+elif letra == "C":
     capitulo = "Capítulo II: Neoplasias"
-elif letra == 'D':
+elif letra == "D":
     if bloque <= 48:
         capitulo = "Capítulo II: Neoplasias"
     else:
         capitulo = "Capítulo III: Enfermedades de la sangre y de los órganos hematopoyéticos y ciertos trastornos que afectan el mecanismo de la inmunidad"
-elif letra == 'E':
+elif letra == "E":
     capitulo = "Capítulo IV: Enfermedades endocrinas, nutricionales y metabólicas"
-elif letra == 'F':
+elif letra == "F":
     capitulo = "Capítulo V: Trastornos mentales y del comportamiento"
-elif letra == 'G':
+elif letra == "G":
     capitulo = "Capítulo VI: Enfermedades del sistema nervioso"
-elif letra == 'H':
+elif letra == "H":
     if bloque <= 59:
         capitulo = "Capítulo VII: Enfermedades del ojo y sus anexos"
     else:
         capitulo = "Capítulo VIII: Enfermedades del oído y de la apófisis mastoides"
-elif letra == 'I':
+elif letra == "I":
     capitulo = "Capítulo IX: Enfermedades del sistema circulatorio"
-elif letra == 'J':
+elif letra == "J":
     capitulo = "Capítulo X: Enfermedades del sistema respiratorio"
-elif letra == 'K':
+elif letra == "K":
     capitulo = "Capítulo XI: Enfermedades del sistema digestivo"
-elif letra == 'L':
+elif letra == "L":
     capitulo = "Capítulo XII: Enfermedades de la piel y del tejido subcutáneo"
-elif letra == 'M':
+elif letra == "M":
     capitulo = "Capítulo XIII: Enfermedades del sistema osteomuscular y del tejido conjuntivo"
-elif letra == 'N':
+elif letra == "N":
     capitulo = "Capítulo XIV: Enfermedades del sistema genitourinario"
-elif letra == 'O':
+elif letra == "O":
     capitulo = "Capítulo XV: Embarazo, parto y puerperio"
-elif letra == 'P':
+elif letra == "P":
     capitulo = "Capítulo XVI: Ciertas afecciones originadas en el período perinatal"
-elif letra == 'Q':
+elif letra == "Q":
     capitulo = "Capítulo XVII: Malformaciones congénitas, deformidades y anomalías cromosómicas"
-elif letra == 'R':
+elif letra == "R":
     capitulo = "Capítulo XVIII: Síntomas, signos y hallazgos anormales clínicos y de laboratorio, no clasificados en otra parte"
-elif letra == 'S' or letra == 'T':
+elif letra == "S" or letra == "T":
     capitulo = "Capítulo XIX: Traumatismos, envenenamientos y algunas otras consecuencias de causas externas"
-elif letra == 'V' or letra == 'W' or letra == 'X' or letra == 'Y':
+elif letra == "V" or letra == "W" or letra == "X" or letra == "Y":
     capitulo = "Capítulo XX: Causas externas de morbilidad y de mortalidad"
-elif letra == 'Z':
+elif letra == "Z":
     capitulo = "Capítulo XXI: Factores que influyen en el estado de salud y contacto con los servicios de salud"
-elif letra == 'U':
+elif letra == "U":
     capitulo = "Capítulo XXII: Códigos para propósitos especiales"
 
     
-
+print("Beneficiario:", beneficiario)
+print("Codigo:", codigo_icd10)
+print("Capitulo:", capitulo)
+print("Monto a pagar:", monto_base)
